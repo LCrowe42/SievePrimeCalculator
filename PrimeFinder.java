f5e016c9cfd9aa819d76e8ae56027941093c4470
LCrowe42 <LNCrowe@outlook.com>
2026-01-01 21:51:25 -0500

    Added additional I/O and the option to only print the largest prime identified along with the time taken by the algorithm

--- PrimeFinder.java
+++ PrimeFinder.java
@@ -9,20 +9,35 @@ public class PrimeFinder {
                 System.out.println("Please enter a number \"n\" greater than 1 to print a list of all primes between 0 and \"n\":");
                 n = scan.nextInt();
             }
+            String choice = "X";
+            do {
+                System.out.println("Please enter \"Y\" to print the primes or \"N\" to just see the largest and processing time");
+                choice = scan.next().toUpperCase();
+            } while (!choice.contains("Y") && !choice.contains("N"));
             boolean[] sieve = new boolean[n + 1];
             long startTime = System.nanoTime(); 
             markAllComposites(sieve);
             long endTime = System.nanoTime();
-            for (int i = 2; i < sieve.length; i++) {
-                if (!sieve[i]) {
-                    System.out.print(i + " ");
+            if (choice.equals("Y")) {
+                for (int i = 2; i < sieve.length; i++) {
+                    if (!sieve[i]) {
+                        System.out.print(i + " ");
+                    }
+                }
+            }
+            else {
+                for (int i = n; i >= 2; i--) {
+                    if (!sieve[i]) {
+                        System.out.print(i);
+                        break;
+                    }
                 }
             }
             System.out.println();
             System.out.println("Primes generated in " + ((endTime - startTime) / 1000000) + "ms");
             scan.close();
         } catch (Exception e) {
-            System.out.println("Error with Scanner");
+            System.out.println(e);
         }
     }
 
